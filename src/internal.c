95a8713e159db99656146bea5e43033dd82a0f7c
Mathijs Follon <mathijs.follon@gmail.com>
2026-08-18 23:16:13 +0200

    Add internal config getter and event emit helpers.

--- src/internal.c
+++ src/internal.c
@@ -1,9 +1,11 @@
 #include "internal.h"
 #include "config.h"
+#include "port.h"
 #include "types.h"
 #include <string.h>
 
 static fdir_config_t g_config;
+
 
 void fdir_internal_set_config(const fdir_config_t *config)
 {
@@ -13,6 +15,12 @@ void fdir_internal_set_config(const fdir_config_t *config)
     }
     g_config = *config;
 }
+
+fdir_config_t* fdir_internal_get_config(void)
+{
+    return &g_config;
+}
+
 
 
 void fdir_internal_copy_detail(char *dst, size_t dst_len, const char *src)
@@ -28,4 +36,30 @@ void fdir_internal_copy_detail(char *dst, size_t dst_len, const char *src)
 
     (void)strncpy(dst, src, dst_len - 1U);
     dst[dst_len - 1U] = '\0';
+}
+
+
+void fdir_internal_emit_mode(fdir_event_kind_t kind, fdir_mode_t mode,
+                             fdir_entity_id_t entity, fdir_reason_t reason,
+                             uint16_t error_code, const char *detail)
+{
+    fdir_event_t event;
+
+    memset(&event, 0, sizeof(event));
+    event.kind = kind;
+    event.mode = mode;
+    event.entity = entity;
+    event.reason = reason;
+    event.error_code = error_code;
+    event.timestamp_ms = fdir_get_now_ms();
+    fdir_internal_copy_detail(event.detail, sizeof(event.detail), detail);
+    fdir_emit_event(&event);
+}
+
+void fdir_internal_emit(fdir_event_kind_t kind, fdir_entity_id_t entity,
+                        fdir_reason_t reason, uint16_t error_code,
+                        const char *detail)
+{
+    fdir_internal_emit_mode(kind, FDIR_MODE_NOMINAL, entity, reason, error_code,
+                            detail);
 }
--- src/internal.h
+++ src/internal.h
@@ -10,13 +10,20 @@ extern "C" {
 
 void fdir_internal_set_config(const fdir_config_t *config);
 
-void fdir_internal_copy_detail(char *dst, size_t dst_len, const char *src);
+fdir_config_t* fdir_internal_get_config(void);
 
-void fdir_internal_set_entity_limit(uint8_t count);
+void fdir_internal_copy_detail(char *dst, size_t dst_len, const char *src);
 
 void fdir_subsystems_init(void);
 
 void fdir_health_init(void);
+
+void fdir_health_set_entity_limit(uint8_t limit);
+
+void fdir_internal_emit(fdir_event_kind_t kind, fdir_entity_id_t entity, fdir_reason_t reason, uint16_t error_code, const char *detail);
+
+void fdir_internal_emit_mode(fdir_event_kind_t kind, fdir_mode_t mode, fdir_entity_id_t entity, fdir_reason_t reason, uint16_t error_code, const char *detail);
+
 
 #ifdef __cplusplus
 }
