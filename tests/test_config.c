c38f5adfc10190f1fa9eeb854d692920d96092e0
Mathijs Follon <mathijs.follon@gmail.com>
2026-08-18 23:17:34 +0200

    Replace add stub with per-module test files.

---
+++ tests/test_config.c
@@ -0,0 +1,19 @@
+#include "test_harness.h"
+
+#include "fdir.h"
+
+TEST(test_config)
+{
+    // Sanity check to test the test_harness
+    fdir_config_t config = fdir_config_default();
+
+    ASSERT_EQ_INT(config.health_check_period_ms, 500U);
+    ASSERT_EQ_INT(config.missed_heartbeat_tolerance, 3U);
+    ASSERT_EQ_INT(config.safe_mode_critical_failure_threshold, 2U);
+}
+
+int main(void)
+{
+    RUN(test_config);
+    return test_harness_summary();
+}
